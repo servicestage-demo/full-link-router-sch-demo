1d48dd88fbdd86fdbad6508f32322046beec9231
Ferko <151807930+moumangtaiwqd@users.noreply.github.com>
2023-12-12 19:23:53 +0800

    添加全链路灰度Demo

---
+++ unit-provider/src/main/java/com/huaweicloud/servicestage/demo/controller/ProviderController.java
@@ -0,0 +1,37 @@
+package com.huaweicloud.servicestage.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * controller
+ *
+ * @author provenceee
+ * @since 2023-07-25
+ */
+@RestController
+public class ProviderController {
+    @Value("${spring.application.name}")
+    private String name;
+
+    @Value("${SERVICECOMB_INSTANCE_PROPS:}")
+    private String props;
+
+    /**
+     * 测试方法
+     *
+     * @return msg
+     */
+    @GetMapping("unit-provider/hello")
+    public Map<String, Object> hello() {
+        Map<String, String> msg = new HashMap<>();
+        msg.put("SERVICECOMB_INSTANCE_PROPS", props);
+        Map<String, Object> map = new HashMap<>();
+        map.put(name, msg);
+        return map;
+    }
+}
