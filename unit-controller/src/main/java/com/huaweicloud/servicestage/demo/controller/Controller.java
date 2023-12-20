8d7f71a1ce2e4de69f699ff2b5925352784466f6
Ferko <151807930+moumangtaiwqd@users.noreply.github.com>
2023-12-20 19:49:31 +0800

    Update Controller.java

--- unit-controller/src/main/java/com/huaweicloud/servicestage/demo/controller/Controller.java
+++ unit-controller/src/main/java/com/huaweicloud/servicestage/demo/controller/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
